bfdab1e8d95cc6f316d1e67799b2f40b1f4db9bb
Matthew Congrove <mattcongrove@gmail.com>
2014-01-01 01:51:47 -0500

    Temporary...

--- src/flying_blind.c
+++ src/flying_blind.c
@@ -31,10 +31,10 @@ static void set_theme() {
 
 static void set_vibrate() {
 	if (persist_exists(KEY_VIBRATE)) {
-		TIME_DISPLAY = persist_read_int(KEY_VIBRATE);
+		VIBRATE = persist_read_int(KEY_VIBRATE);
 	}
 	
-	APP_LOG(APP_LOG_LEVEL_INFO, "SELECTED VIBRATE: %d", TIME_DISPLAY);
+	APP_LOG(APP_LOG_LEVEL_INFO, "SELECTED VIBRATE: %d", VIBRATE);
 }
 
 static void in_received_handler(DictionaryIterator *iter, void *context) {
@@ -63,10 +63,72 @@ static void in_dropped_handler(AppMessageResult reason, void *context) {
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-	time_t t = time(NULL);
-	struct tm *now = localtime(&t);
+	time_t now = time(NULL);
+	struct tm *t = localtime(&now);
 	
+	int tHour = 5;
+	int tMin = 0;
 	
+	uint32_t segments[25];
+	segments[0] = 100;
+	
+	if (tMin == 0) {
+		if (tHour == 1) {
+				static const uint32_t const segments1[] = { 500, 100, 100 };
+				VibePattern pat = { .durations = segments1, .num_segments = ARRAY_LENGTH(segments1) };
+		} else if (tHour == 2) {
+				static const uint32_t const segments2[] = { 500, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments2, .num_segments = ARRAY_LENGTH(segments2) };
+		} else if (tHour == 3) {
+				static const uint32_t const segments3[] = { 500, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments3, .num_segments = ARRAY_LENGTH(segments3) };
+		} else if (tHour == 4) {
+				static const uint32_t const segments4[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments4, .num_segments = ARRAY_LENGTH(segments4) };
+		} else if (tHour == 5) {
+				static const uint32_t const segments5[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments5, .num_segments = ARRAY_LENGTH(segments5) };
+		} else if (tHour == 6) {
+				static const uint32_t const segments6[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments6, .num_segments = ARRAY_LENGTH(segments6) };
+		} else if (tHour == 7) {
+				static const uint32_t const segments7[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments7, .num_segments = ARRAY_LENGTH(segments7) };
+		} else if (tHour == 8) {
+				static const uint32_t const segments8[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments8, .num_segments = ARRAY_LENGTH(segments8) };
+		} else if (tHour == 9) {
+				static const uint32_t const segments9[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments9, .num_segments = ARRAY_LENGTH(segments9) };
+		} else if (tHour == 10) {
+				static const uint32_t const segments10[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments10, .num_segments = ARRAY_LENGTH(segments10) };
+		} else if (tHour == 11) {
+				static const uint32_t const segments11[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments11, .num_segments = ARRAY_LENGTH(segments11) };
+		} else if (tHour == 12) {
+				static const uint32_t const segments12[] = { 500, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+				VibePattern pat = { .durations = segments12, .num_segments = ARRAY_LENGTH(segments12) };
+		}
+	}
+	
+	if ((VIBRATE == 1 && tMin == 30) || (VIBRATE == 2 && (tMin == 15 || tMin == 30 || tMin == 45))) {
+		if (tMin == 15) {
+			static const uint32_t const segments15[] = { 100 };
+		}
+		
+		if (tMin == 30) {
+			static const uint32_t const segments30[] = { 100, 100, 100 };
+		}
+		
+		if (tMin == 45) {
+			static const uint32_t const segments45[] = { 100, 100, 100, 100, 100, 100 };
+		}
+	}
+	
+	VibePattern pat = { .durations = segments, .num_segments = ARRAY_LENGTH(segments) };
+	
+	vibes_enqueue_custom_pattern(pat);
 }
 
 static void init() {
